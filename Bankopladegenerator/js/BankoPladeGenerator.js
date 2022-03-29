15db4b89e706df4f507bac8c5ba5f60ebc4a0acf
Asgersp03 <50337738+Asgersp03@users.noreply.github.com>
2022-03-29 22:03:49 +0200

    OMG Banko generatoren fungerer nu!
    Nu er bankogeneratoren mere eller mindre færdig. Den kan produceret et layout og udfylde dette med korrekte numre. Ahh hvor dejligt

--- Bankopladegenerator/js/BankoPladeGenerator.js
+++ Bankopladegenerator/js/BankoPladeGenerator.js
@@ -1,6 +1,6 @@
 let pladeUdkast =[
   [1,1,1,1,1,1,1,1,1],
-  [1,1,1,1,1,1,1,1,1],
+  [1,1,1,1,1,1,1,1,],
   [1,1,1,1,1,1,1,1,1]
 ];
 
@@ -12,8 +12,8 @@ let plade = [
 
 function generer(){
   function layout(){
-    let k=0;
-    //k er række nummeret
+    let r=0;
+    //r er række nummeret
     let j=[];
     //j er et array hvori de udtrukkede værdier af i gemmes, for at undgå dubletter.
     let i=Math.floor(Math.random()*9);
@@ -29,7 +29,7 @@ function generer(){
           /*Hvis der udtrækkes en værdi af i der i forvejen er gemt i j, udrtækkes der en ny værdi af i.*/
         }
         else {
-          pladeUdkast[k][i]=0;
+          pladeUdkast[r][i]=0;
           j.push(i);
           //i værdien placeres i j arrayet
         }
@@ -37,47 +37,281 @@ function generer(){
       return(pladeUdkast)
     }
 
-    while(k<3){
-      placerNul(pladeUdkast, k, i, j)
-      k=k+1;
+    while(r<3){
+      placerNul(pladeUdkast, r, i, j)
+      r++;
       j=[];
       // Dette while loop lader der blive placeret 4 nuller i hver række, indtil alle tre rækker er udfyldt.
     }
     return(pladeUdkast)
   }
-
   function tal(){
+    let r=0;
+    //r er rækkenummeret
+    let s=0;
+    //s er søjlenummert
+    let j=[];
+    //j er et array hvortil søjlerne midlertidigt kopieres.
+    let i=1;
+    //i er den variable hvori de tilfældigt genrerede tal vil blive gemt.
+    let q=[];
+    //q bruges som en cache til at undgå dubletter i de generede tilfældige tal.
 
+    function kopierSoljeTal(){
+      while(r<3){
+        j.push(pladeUdkast[r][s]);
+        r++;
+      }
+      r=0;
+      return(j);
+    }
+    //Denne funktion kopierer søjle s over i j
+    while(s<9){
+      if(s==0){
+        kopierSoljeTal(r,s,j);
+        let l = j.filter(x => x==1).length;
+        for(let z=0; z<l; z++){
+          i=Math.floor(Math.random() * (9 - 1 + 1) ) + 1;
+          if(q.includes(i)){
+            i=Math.floor(Math.random() * (9 - 1 + 1) ) + 1;
+          }
+          else{
+            q.push(i);
+          }
+        }
+        q=q.sort();
+        for(let r=0; r<3; r++){
+          if(pladeUdkast[r][s]==1){
+            pladeUdkast[r][s]=q.slice(0,1);
+            q.shift();
+          }
+        }
+        r=0;
+        j=[];
+        s++;
+      }
+      if(s==1){
+        kopierSoljeTal(r,s,j);
+        let l = j.filter(x => x==1).length;
+        for(let z=0; z<l; z++){
+          i=Math.floor(Math.random() * (19 - 10 + 1) ) + 10;
+          if(q.includes(i)){
+            i=Math.floor(Math.random() * (19 - 10 + 1) ) + 10;
+          }
+          else{
+            q.push(i);
+          }
+        }
+        q=q.sort();
+        for(let r=0; r<3; r++){
+          if(pladeUdkast[r][s]==1){
+            pladeUdkast[r][s]=q.slice(0,1);
+            q.shift();
+          }
+        }
+        r=0;
+        j=[];
+        s++;
+      }
+      if(s==2){
+        kopierSoljeTal(r,s,j);
+        let l = j.filter(x => x==1).length;
+        for(let z=0; z<l; z++){
+          i=Math.floor(Math.random() * (29 - 20 + 1) ) + 20;
+          if(q.includes(i)){
+            i=Math.floor(Math.random() * (29 - 20 + 1) ) + 20;
+          }
+          else{
+            q.push(i);
+          }
+        }
+        q=q.sort();
+        for(let r=0; r<3; r++){
+          if(pladeUdkast[r][s]==1){
+            pladeUdkast[r][s]=q.slice(0,1);
+            q.shift();
+          }
+        }
+        r=0;
+        j=[];
+        s++;
+      }
+      if(s==3){
+        kopierSoljeTal(r,s,j);
+        let l = j.filter(x => x==1).length;
+        for(let z=0; z<l; z++){
+          i=Math.floor(Math.random() * (39 - 30 + 1) ) + 30;
+          if(q.includes(i)){
+            i=Math.floor(Math.random() * (39 - 30 + 1) ) + 30;
+          }
+          else{
+            q.push(i);
+          }
+        }
+        q=q.sort();
+        for(let r=0; r<3; r++){
+          if(pladeUdkast[r][s]==1){
+            pladeUdkast[r][s]=q.slice(0,1);
+            q.shift();
+          }
+        }
+        r=0;
+        j=[];
+        s++;
+      }
+      if(s==4){
+        kopierSoljeTal(r,s,j);
+        let l = j.filter(x => x==1).length;
+        for(let z=0; z<l; z++){
+          i=Math.floor(Math.random() * (49 - 40 + 1) ) + 40;
+          if(q.includes(i)){
+            i=Math.floor(Math.random() * (49 - 40 + 1) ) + 40;
+          }
+          else{
+            q.push(i);
+          }
+        }
+        q=q.sort();
+        for(let r=0; r<3; r++){
+          if(pladeUdkast[r][s]==1){
+            pladeUdkast[r][s]=q.slice(0,1);
+            q.shift();
+          }
+        }
+        r=0;
+        j=[];
+        s++;
+      }
+      if(s==5){
+        kopierSoljeTal(r,s,j);
+        let l = j.filter(x => x==1).length;
+        for(let z=0; z<l; z++){
+          i=Math.floor(Math.random() * (59 - 50 + 1) ) + 50;
+          if(q.includes(i)){
+            i=Math.floor(Math.random() * (59 - 50 + 1) ) + 50;
+          }
+          else{
+            q.push(i);
+          }
+        }
+        q=q.sort();
+        for(let r=0; r<3; r++){
+          if(pladeUdkast[r][s]==1){
+            pladeUdkast[r][s]=q.slice(0,1);
+            q.shift();
+          }
+        }
+        r=0;
+        j=[];
+        s++;
+      }
+      if(s==6){
+        kopierSoljeTal(r,s,j);
+        let l = j.filter(x => x==1).length;
+        for(let z=0; z<l; z++){
+          i=Math.floor(Math.random() * (69 - 60 + 1) ) + 60;
+          if(q.includes(i)){
+            i=Math.floor(Math.random() * (69 - 60 + 1) ) + 60;
+          }
+          else{
+            q.push(i);
+          }
+        }
+        q=q.sort();
+        for(let r=0; r<3; r++){
+          if(pladeUdkast[r][s]==1){
+            pladeUdkast[r][s]=q.slice(0,1);
+            q.shift();
+          }
+        }
+        r=0;
+        j=[];
+        s++;
+      }
+      if(s==7){
+        kopierSoljeTal(r,s,j);
+        let l = j.filter(x => x==1).length;
+        for(let z=0; z<l; z++){
+          i=Math.floor(Math.random() * (79 - 70 + 1) ) + 70;
+          if(q.includes(i)){
+            i=Math.floor(Math.random() * (79 - 70 + 1) ) + 70;
+          }
+          else{
+            q.push(i);
+          }
+        }
+        q=q.sort();
+        for(let r=0; r<3; r++){
+          if(pladeUdkast[r][s]==1){
+            pladeUdkast[r][s]=q.slice(0,1);
+            q.shift();
+          }
+        }
+        r=0;
+        j=[];
+        s++;
+      }
+      if(s==8){
+        kopierSoljeTal(r,s,j);
+        let l = j.filter(x => x==1).length;
+        for(let z=0; z<l; z++){
+          i=Math.floor(Math.random() * (90 - 80 + 1) ) + 80;
+          if(q.includes(i)){
+            i=Math.floor(Math.random() * (90 - 80 + 1) ) + 80;
+          }
+          else{
+            q.push(i);
+          }
+        }
+        q=q.sort();
+        for(let r=0; r<3; r++){
+          if(pladeUdkast[r][s]==1){
+            pladeUdkast[r][s]=q.slice(0,1);
+            q.shift();
+          }
+        }
+        r=0;
+        j=[];
+        s++;
+      }
+    }
+    s=0;
+    return(pladeUdkast);
   }
   layout(pladeUdkast);
   tjekLayout(pladeUdkast);
+  while(tjekLayout()==false){
+    kasserUdkast(pladeUdkast);
+    layout(pladeUdkast);
+  }
+  tal(pladeUdkast);
   return(pladeUdkast)
 }
 
 function tjekLayout(){
-  let i=0;
+  let s=0;
   //i er søjle koordinaten
-  let k=0;
+  let r=0;
   //k er række koordinaten
   let j=[];
   //j er et array hvortil søjlerne midlertidigt kopieres
 
   function kopierSojle(){
-      while(k<3){
-        j.push(pladeUdkast[k][i])
-        k=k+1
+      while(r<3){
+        j.push(pladeUdkast[r][s]);
+        r++;
       }
     return(j)
   }
   //Denne funktion kopierer de tre tal i søjle i, over i j arrayet.
 
-  while(i<9){
-    kopierSojle(j,k,i);
+  while(s<9){
+    kopierSojle(j,r,s);
     //Søjle i kopieres til j
     if(j.filter(x => x==0).length < 3){
     //Hvis antallet af nuller i j er mindre end 3, nulstilles k og der rykkes en søjle frem vis i=i+1
-      k=0;
-      i=i+1;
+      r=0;
+      s++;
       j=[];
     }
     else{
@@ -86,12 +320,6 @@ function tjekLayout(){
   }
   return true
   //Hvis alle 9 søjler er tjekket igennem, og der i ingen af dem var 3 nuller, returneres true
-}
-
-function tjekTal(){
-
-
-  return(pladeUdkast)
 }
 
 function udskriv(){
@@ -103,7 +331,7 @@ function udskriv(){
     document.getElementById('0.0').innerHTML=" "
   }
   else{
-    document.getElementById('0.0').innerHTML=plade[0][0];
+      document.getElementById('0.0').innerHTML=plade[0][0];
   }
 
   if(plade[0][1]==0){
@@ -287,6 +515,7 @@ function udskriv(){
   else{
     document.getElementById('2.8').innerHTML=plade[2][8];
   }
+
 }
 
 function kasserUdkast(){
@@ -300,9 +529,5 @@ function kasserUdkast(){
 
 function lavPlade(){
   generer(pladeUdkast);
-  while(tjekLayout()==false){
-    kasserUdkast(pladeUdkast);
-    generer(pladeUdkast);
-  }
   udskriv(plade);
-  }
+}
